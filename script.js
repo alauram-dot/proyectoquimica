3ea4b45d6de3051fddde0d4ffdc0a0a83f3eaccc
alauram-dot <alauram@escuelamilitar.edu.pe>
2026-06-09 10:22:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,75 +1,47 @@
-body{
-margin:0;
-display:flex;
-font-family:Segoe UI;
-background:#0a0f0a;
-color:white;
+const tabla =
+document.getElementById("tabla");
+
+function mostrar(lista){
+
+tabla.innerHTML="";
+
+lista.forEach(el=>{
+
+const div =
+document.createElement("div");
+
+div.className="elemento";
+
+div.innerHTML=`
+<h3>${el.simbolo}</h3>
+<p>${el.nombre}</p>
+<small>#${el.numero}</small>
+`;
+
+tabla.appendChild(div);
+
+});
+
 }
 
-.sidebar{
-width:250px;
-height:100vh;
-background:#1b2b1b;
-padding:20px;
-position:fixed;
-}
+mostrar(elementos);
 
-.sidebar ul{
-list-style:none;
-padding:0;
-}
+document
+.getElementById("buscador")
+.addEventListener("input",e=>{
 
-.sidebar li{
-padding:12px;
-margin:10px 0;
-background:#263826;
-border-radius:8px;
-cursor:pointer;
-}
+const texto =
+e.target.value.toLowerCase();
 
-.main{
-margin-left:270px;
-padding:20px;
-width:100%;
-}
+const filtrado =
+elementos.filter(el=>
 
-.stats{
-display:flex;
-gap:20px;
-}
+el.nombre.toLowerCase().includes(texto)
+||
+el.simbolo.toLowerCase().includes(texto)
 
-.card{
-background:#1f3320;
-padding:20px;
-border-radius:12px;
-width:180px;
-}
+);
 
-#buscador{
-width:100%;
-padding:12px;
-margin:20px 0;
-font-size:16px;
-border:none;
-border-radius:8px;
-}
+mostrar(filtrado);
 
-#tabla{
-display:grid;
-grid-template-columns:
-repeat(auto-fill,minmax(120px,1fr));
-gap:15px;
-}
-
-.elemento{
-background:#355735;
-padding:15px;
-border-radius:10px;
-cursor:pointer;
-transition:.3s;
-}
-
-.elemento:hover{
-transform:scale(1.05);
-background:#4b754b;
-}
+});
